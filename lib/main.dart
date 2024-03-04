5ed2678cb1f4dac13eab1c1f1c43eb0e30e1437f
triiikusuma <trikusumaa06@gmail.com>
2024-03-04 15:34:52 +0800

    c. Menggunakan Scaffold dan AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,15 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Praktikum Layout Flutter'),
+      ),
+      body: //
+    );
+  }
+}
