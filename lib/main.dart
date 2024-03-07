0ac0cc0e8f7de8653b10f6faedefb9efb5620007
triiikusuma <trikusumaa06@gmail.com>
2024-03-07 12:56:41 +0800

    g. mengubah tittle pada MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Praktikum Layout Praktikum',
+      title: 'Flutter Layout Praktikum',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         primarySwatch: Colors.blue,
